84bc77f70e6d421d5b0d20ed8342bf842029f6fe
MiracleSkyHawk <jesse.yang.uoft@gmail.com>
2021-03-27 18:40:01 -0400

    create folder

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,23 @@
+console.log("working");
+
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    fillColor: "#ffffa1",
+    color: "black"
+
+}).addTo(map);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+})
+
+streets.addTo(map);
+
+// mapbox://styles/mapbox/dark-v10
