6898ba87eb0f71372d4a5cca6c5ebfb231aa9c48
MiracleSkyHawk <jesse.yang.uoft@gmail.com>
2021-03-27 18:36:55 -0400

    yellow circle

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,9 +1,18 @@
 console.log("working");
 
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    fillColor: "#ffffa1",
+    color: "black"
+
+}).addTo(map);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -11,3 +20,4 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 streets.addTo(map);
 
+// mapbox://styles/mapbox/dark-v10
