94a34cae3f90e812e869eb2a449b33d84730428a
MiracleSkyHawk <jesse.yang.uoft@gmail.com>
2021-03-27 23:01:03 -0400

    lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,40 +1,25 @@
 console.log("working");
 
-let map = L.map("mapid").setView([34.0522, -118.2437], 14);
-
-let cityData = cities;
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        fillColor: "orange",
-        color: "orange",
-        weight: 4
-    })
-    .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// Coordinates for each point to be used in the line.
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781],
 
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location)
-//     .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//     .addTo(map);
-// });
 
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+  ];
 
-// L.circleMarker([34.0522, -118.2437], {
-//     radius: 300,
-//     fillColor: "#ffffa1",
-//     color: "black"
+L.polyline(line, {
+    color: "blue",
+    dashArray: [4, 12],
+    weight: 4
+}).addTo(map);
 
-// }).addTo(map);
-
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -42,4 +27,5 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 
 streets.addTo(map);
 
-// mapbox://styles/mapbox/dark-v10
+// satellite-streets-v11
+// dark-v10
