4df8bd4911f429b5b0d48e5ed2c2583194da7876
MiracleSkyHawk <jesse.yang.uoft@gmail.com>
2021-03-27 22:21:34 -0400

    bind popup

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,14 +3,14 @@ console.log("working");
 let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 
 //  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    fillColor: "#ffffa1",
-    color: "black"
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     fillColor: "#ffffa1",
+//     color: "black"
 
-}).addTo(map);
+// }).addTo(map);
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
